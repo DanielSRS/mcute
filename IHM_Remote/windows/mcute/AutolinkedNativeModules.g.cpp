bd6e678ad7d71eaaeb2989c693da90f17b0bb86a
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-12-04 14:38:28 -0300

    🚧➕ Add react-native-linear-gradient

--- IHM_Remote/windows/mcute/AutolinkedNativeModules.g.cpp
+++ IHM_Remote/windows/mcute/AutolinkedNativeModules.g.cpp
@@ -3,6 +3,9 @@
 #include "pch.h"
 #include "AutolinkedNativeModules.g.h"
 
+// Includes from react-native-linear-gradient
+#include <winrt/BVLinearGradient.h>
+
 // Includes from react-native-screens
 #include <winrt/RNScreens.h>
 
@@ -14,6 +17,8 @@ namespace winrt::Microsoft::ReactNative
 
 void RegisterAutolinkedNativeModulePackages(winrt::Windows::Foundation::Collections::IVector<winrt::Microsoft::ReactNative::IReactPackageProvider> const& packageProviders)
 { 
+    // IReactPackageProviders from react-native-linear-gradient
+    packageProviders.Append(winrt::BVLinearGradient::ReactPackageProvider());
     // IReactPackageProviders from react-native-screens
     packageProviders.Append(winrt::RNScreens::ReactPackageProvider());
     // IReactPackageProviders from react-native-svg
