d0a595c0f0ce70d1dad427b0b644a3eda57e471b
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2022-12-10 23:39:42 -0300

    🪲➕ Fix react-native-linear-gradient on web
    close #4

--- IHM_Remote/web/webpack.config.js
+++ IHM_Remote/web/webpack.config.js
@@ -80,6 +80,7 @@ module.exports = {
     alias: {
       'react-native$': 'react-native-web',
       'react-native-webview': 'react-native-web-webview',
+      'react-native-linear-gradient': 'react-native-web-linear-gradient',
     },
     // If you're working on a multi-platform React Native app, web-specific
     // module implementations should be written in files using the extension
